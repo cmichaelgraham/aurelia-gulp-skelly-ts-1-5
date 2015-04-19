5c38d8b316947620e4bb33caf68d635d83776b2a
Mike Graham <mgraham@technicalmedia.com>
2015-04-19 16:46:52 -0600

    cycle working without combining files

--- bundler/bundle-and-copy-to-solutions.sh
+++ bundler/bundle-and-copy-to-solutions.sh
@@ -25,7 +25,7 @@ cd aurelia-latest
 ../clone-and-build.sh aurelia http-client
 ../clone-and-build.sh aurelia bootstrapper
 ../clone-and-build.sh aurelia html-template-element
-../clone-and-build.sh cmichaelgraham templating
+../clone-and-build.sh technicalmedia templating
 
 cd ..
 
--- views/main.js
+++ views/main.js
@@ -1,11 +1,3 @@
-var __decorate = this.__decorate || (typeof Reflect === "object" && Reflect.decorate) || function (decorators, target, key, desc) {
-    switch (arguments.length) {
-        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
-        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
-        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
-    }
-};
-var __metadata = this.__metadata || (typeof Reflect === "object" && Reflect.metadata) || function () { };
 define(["require", "exports", 'aurelia-framework', 'aurelia-logging-console'], function (require, exports, aurelia_framework_1, aurelia_logging_console_1) {
     aurelia_framework_1.LogManager.addAppender(new aurelia_logging_console_1.ConsoleAppender());
     aurelia_framework_1.LogManager.setLevel(4 /* debug */);
